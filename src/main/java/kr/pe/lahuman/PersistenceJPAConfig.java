4f8630beac70cabaeab96f2ed4b9dcc45725e7a1
lahuman <lahuman1@gmail.com>
2015-07-17 15:23:06 +0900

    with DBPOOL

---
+++ src/main/java/kr/pe/lahuman/PersistenceJPAConfig.java
@@ -0,0 +1,88 @@
+package kr.pe.lahuman;
+
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * Created by lahuman on 2015-07-17.
+ */
+@Configuration
+@EnableTransactionManagement
+public class PersistenceJPAConfig {
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws PropertyVetoException, SQLException {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(new String[] { "kr.pe.lahuman" });
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
+
+        return em;
+    }
+
+    @Bean
+    public DataSource dataSource() throws PropertyVetoException, SQLException {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.h2.Driver");
+//        dataSource.setUrl("jdbc:h2:~/lahuman;DB_CLOSE_ON_EXIT=FALSE");
+//        dataSource.setUsername( "sa" );
+//        dataSource.setPassword( "" );
+
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass("org.h2.Driver");
+        dataSource.setJdbcUrl("jdbc:h2:~/lahuman;DB_CLOSE_ON_EXIT=FALSE");
+        dataSource.setUser("sa");
+        dataSource.setPassword("");
+
+        //pool properties
+        dataSource.setMinPoolSize(5);
+        dataSource.setMaxPoolSize(20);
+        dataSource.setAcquireIncrement(1);
+        dataSource.setMaxStatements(50);
+        dataSource.setIdleConnectionTestPeriod(3000);
+        dataSource.setLoginTimeout(300);
+
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+
+        return transactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+//        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.format_sql", "true");
+        return properties;
+    }
+}
